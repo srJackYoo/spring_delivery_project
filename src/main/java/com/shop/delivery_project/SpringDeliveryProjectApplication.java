d82bf0d0c568b0d1ee484e416fcbdfe196ca103c
SRYoo <yosr94@naver.com>
2022-05-13 12:32:11 +0900

    First Upload

---
+++ src/main/java/com/shop/delivery_project/SpringDeliveryProjectApplication.java
@@ -0,0 +1,13 @@
+package com.shop.delivery_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDeliveryProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDeliveryProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shop/delivery_project/SpringDeliveryProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.delivery_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDeliveryProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
